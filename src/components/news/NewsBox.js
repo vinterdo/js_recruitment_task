aafe78e2349020eec4dc6f5e95fe6a21e8224a3d
Piotr Brzezny <pbrzezny@opera.com>
2020-09-27 01:20:57 +0200

    Open in new window

--- src/components/news/NewsBox.js
+++ src/components/news/NewsBox.js
@@ -2,21 +2,20 @@ import { dispatch, getState } from '../../state/state';
 import addToReadLater from '../../state/actions/addToReadLater';
 import { onClick } from '../../renderer';
 
-
 const NewsBox = ({ id, title, sectionName, publicationDate, articleLink }) => {
-    const state = getState();
+  const state = getState();
 
-    onClick(
-        () => document.querySelector(`#${id} .read-later-button`),
-        onReadLater
-    );
-    function onReadLater() {
-        dispatch(addToReadLater(id));
-    }
+  onClick(
+    () => document.querySelector(`#${id} .read-later-button`),
+    onReadLater
+  );
+  function onReadLater() {
+    dispatch(addToReadLater(id));
+  }
 
-    const isInReadLater = state.readLater.map(rl => rl.id).includes(id);
+  const isInReadLater = state.readLater.map((rl) => rl.id).includes(id);
 
-    return `
+  return `
      <li id="${id}">
        <article class="news">
          <header>
@@ -25,12 +24,18 @@ const NewsBox = ({ id, title, sectionName, publicationDate, articleLink }) => {
          <section class="newsDetails">
            <ul>
              <li><strong>Section Name:</strong> ${sectionName}</li>
-             <li><strong>Publication Date:</strong> ${new Date(publicationDate).toLocaleString()}</li>
+             <li><strong>Publication Date:</strong> ${new Date(
+               publicationDate
+             ).toLocaleString()}</li>
            </ul>
          </section>
          <section class="newsActions">
-           <a href="${articleLink}" class="button">Full article</a>
-           ${isInReadLater ? '' : '<button class="button button-outline read-later-button">Read Later</button>'}
+           <a href="${articleLink}" target="_blank" class="button">Full article</a>
+           ${
+             isInReadLater
+               ? ''
+               : '<button class="button button-outline read-later-button">Read Later</button>'
+           }
          </section>
        </article>
      </li>
--- src/components/news/ReadLaterBox.js
+++ src/components/news/ReadLaterBox.js
@@ -3,20 +3,20 @@ import removeReadLater from '../../state/actions/removeReadLater';
 import { onClick } from '../../renderer';
 
 const ReadLaterBox = ({ id, title, articleLink }) => {
-    onClick(
-        () => document.querySelector(`#${id} .remove-read-later-button`),
-        onRemove
-    );
+  onClick(
+    () => document.querySelector(`#${id} .remove-read-later-button`),
+    onRemove
+  );
 
-    function onRemove() {
-        dispatch(removeReadLater(id));
-    }
+  function onRemove() {
+    dispatch(removeReadLater(id));
+  }
 
-    return `
+  return `
         <li id="${id}">
           <h4 class="readLaterItem-title">${title}</h4>
           <section>
-            <a href="${articleLink}" class="button button-clear">Read</a>
+            <a href="${articleLink}" target="_blank" class="button button-clear">Read</a>
             <button class="button button-clear remove-read-later-button">Remove</button>
           </section>
         </li>
